d60d16bd214fe0e06d232415bb1fd83b076ad178
jessus-thread <jessus.thread@outlook.com>
2026-09-03 15:05:25 -0600

    feat: finaliza tarea - input de imágenes

--- 06-gifs-app/src/app/gifs/components/gif-list/gif-list.ts
+++ 06-gifs-app/src/app/gifs/components/gif-list/gif-list.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { GifListItem } from '../gif-list-item/gif-list-item';
 
 @Component({
-  selector: 'app-gif-list',
-  imports: [],
+  selector: 'gif-list',
+  imports: [GifListItem],
   templateUrl: './gif-list.html',
 })
-export class GifList {}
+export class GifList {
+  public readonly gifs = input.required<string[]>();
+}
